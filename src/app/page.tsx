4b8ea68ae002639a31102883bcaf44ce672100e6
David Randoll <51640895+david-randoll@users.noreply.github.com>
2024-10-06 01:02:28 -0400

    add better image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ const Home = () => {
             {!isSupported && (
                 <div className="p-6 w-full max-w-md">
                     <p className="text-red-500 text-center mb-6">Push notifications are not supported in this browser. Consider adding to the home screen (PWA) if on iOS.</p>
-                    <Image src="/pwa-ios.webp" width={10000} height={10000} alt="Push Notification" className="h-auto w-full" />
+                    <Image src="/ios-pwa/pwa_ios.jpg" width={10000} height={10000} alt="Push Notification" className="h-auto w-auto" />
                 </div>
             )}
 
